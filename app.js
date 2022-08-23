16945a28ce1ae40cc836c45f95ca2d49c46b716f
Ryan Longo <ryanlongo20@gmail.com>
2022-08-22 20:36:15 -0400

    utilized switch statement with eval function to make string input from calc display into solvable expression.

--- app.js
+++ app.js
@@ -1,22 +1,91 @@
-function sum(a, b){
-    return a + b;
-}
+// const buttons = Array.from(document.querySelectorAll('button'))
+// const display = document.querySelector('.display')
 
-function subtract(a,b) {
-    return a - b;
-}
+// console.log(buttons);
 
-function multiply(a, b) {
-    return a * b;
-}
 
-function divide(a, b) {
-    return a / b;
-}
+// buttons.map( button => {
+//     button.addEventListener('click', (e) => {
+//         switch(e.target.innerText){
+//             case 'C':
+//                 display.innerText = "";
+//                 break;
+//             case '=':
+//                 try{
+//                     display.innerText = eval(e.target.innerText)
+//                 } catch {
+//                     display.innerText = "Error"
+//                 }
+//                 break;
+//             default:
+//                 display.innerText += e.target.innerText;
+//         }
+//     })
+// });
 
-function operate(operator, a, b) {
-    if (operator === '+') return sum(a,b);
-    else if (operator === '-') return subtract(a,b);
-    else if (operator === '*') return multiply(a,b);
-    else return divide(a,b);
-}
+// buttons.map( button => {
+//     button.addEventListener('click', (e) => {
+//         switch(e.target.innerText){
+//             case 'C':
+//                 display.innerText = '';
+//                 break;
+//             case '=':
+//                 try{
+//                     display.innerText = eval(display.innerText);
+//                 } catch {
+//                     display.innerText = "Error"
+//                 }
+//                 break;
+//             case '←':
+//                 if (display.innerText){
+//                    display.innerText = display.innerText.slice(0, -1);
+//                 }
+//                 break;
+//             default:
+//                 display.innerText += e.target.innerText;
+//         }
+//     });
+// });
+
+const buttons = Array.from(document.querySelectorAll('button'));
+const display = document.querySelector('.display');
+console.log(display);
+console.log(buttons);
+
+buttons.map ( button => {
+    button.addEventListener('click', e => {
+        switch(e.target.innerText) {
+            case 'C':
+                display.innerText = "";
+                break;
+            case '=':
+                try {
+                display.innerText = eval(display.innerText);
+                } catch {
+                display.innerText = "Error"
+                }
+                break;
+            default: 
+                display.innerText += e.target.innerText;
+        }   
+    })
+})
+
+buttons.map ( button => {
+    button.addEventListener('keydown', e => {
+        switch(e.target.innerText) {
+            case 'C':
+                display.innerText = "";
+                break;
+            case '=':
+                try {
+                display.innerText = eval(display.innerText);
+                } catch {
+                display.innerText = "Error"
+                }
+                break;
+            default: 
+                display.innerText += e.target.innerText;
+        }   
+    })
+})
